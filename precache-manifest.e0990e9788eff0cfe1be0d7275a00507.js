f3167b7a3160f3d14cdca02ae94ef745e0e0a31f
Ahmed Rizk <arizk456@gmail.com>
2019-06-29 19:54:21 +0200

    Updates

---
+++ precache-manifest.e0990e9788eff0cfe1be0d7275a00507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d5e7ae3373683cdb4be4f6ed794853",
+    "url": "/OneProject-React/index.html"
+  },
+  {
+    "revision": "6c57478fb040ba1116ee",
+    "url": "/OneProject-React/static/css/main.fc71d57d.chunk.css"
+  },
+  {
+    "revision": "3da92d08e44c437a43cf",
+    "url": "/OneProject-React/static/js/2.54df4bc2.chunk.js"
+  },
+  {
+    "revision": "6c57478fb040ba1116ee",
+    "url": "/OneProject-React/static/js/main.250852ba.chunk.js"
+  },
+  {
+    "revision": "8c86860cc46be105f85a",
+    "url": "/OneProject-React/static/js/runtime~main.f0817553.js"
+  }
+]);
